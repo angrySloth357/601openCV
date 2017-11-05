c312c04b1fc3b2615ac4c8c3bb0009c2656a240b
skhadka90 <shreeya.khadka@gmail.com>
2017-11-05 17:36:31 -0500

    :pig_nose:

--- Threshold.cpp
+++ Threshold.cpp
@@ -18,14 +18,14 @@ int main( int argc, char** argv )
  
   namedWindow("Input Image", CV_WINDOW_AUTOSIZE );
   imshow("Input Image", src);
-
+  waitKey( 0 );
   /* 0: Binary,      1: Binary Inverted,      2: Threshold Truncated,      3: Threshold to Zero,      4: Threshold to Zero Inverted    */
   int threshold_type = 2;    // slider 1 [0, 4]
   int threshold_value = 128;  // slider 2 [0 255]
 
     threshold(gray, dst, threshold_value, 255, threshold_type );
     imshow("Thresholded Image", dst );
-    
+    waitKey( 0 );
          int current_threshold = 128;
         int max_threshold = 255;
 	
@@ -33,7 +33,7 @@ int main( int argc, char** argv )
 	Mat thresholded;
 	threshold(gray, thresholded, current_threshold, max_threshold, THRESH_BINARY);
 	imshow("Binary threshold",thresholded);	
-
+	waitKey( 0 );
 	// Band thresholding
 	Mat binary_image1,binary_image2,band_thresholded_image;
 	int threshold1 = 27, threshold2 = 125;
@@ -41,13 +41,13 @@ int main( int argc, char** argv )
 	threshold(gray, binary_image2, threshold2, max_threshold, THRESH_BINARY_INV);
 	bitwise_and( binary_image1, binary_image2, band_thresholded_image );
         imshow("Band Thresholding", band_thresholded_image);
-
+        waitKey( 0 );
 	// Semi thresholding
 	Mat semi_thresholded_image;
 	threshold(gray, semi_thresholded_image, current_threshold, max_threshold, THRESH_BINARY_INV | THRESH_OTSU);
 	bitwise_and( gray, semi_thresholded_image, semi_thresholded_image );
         imshow("Semi Thresholding", semi_thresholded_image);
-
+    waitKey( 0 );
 	// Adaptive thresholding
 	Mat adaptive_thresh;
 	adaptiveThreshold(gray, adaptive_thresh, 255.0, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 101, 10 );
